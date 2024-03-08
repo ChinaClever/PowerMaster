5058dbc6ad0e821abb038da9c006e5ef1b37b0e7
zhanlinjie <luozhiyong131@qq.com>
2024-03-08 09:32:12 +0800

    PDU模块新建

---
+++ powermaster-module-pdu/powermaster-module-pdu-biz/src/main/java/cn/clever/powermaster/module/pdu/controller/test.java
@@ -0,0 +1,25 @@
+package cn.clever.powermaster.module.pdu.controller;
+
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
+
+@Tag(name = "管理后台 - Test")
+@RestController
+@RequestMapping("/demo/test")
+@Validated
+public class test {
+
+    @GetMapping("/get")
+    @Operation(summary = "获取 test 信息")
+    public CommonResult<String> get() {
+        return success("true");
+    }
+
+}
