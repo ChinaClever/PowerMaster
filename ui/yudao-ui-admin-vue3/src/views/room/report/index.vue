fbccc9f4d957f5128febed4de19eeee7c5c7a79c
zhanlinjie <luozhiyong131@qq.com>
2024-04-03 17:26:20 +0800

    机房报表

--- ui/yudao-ui-admin-vue3/src/views/room/report/index.vue
+++ ui/yudao-ui-admin-vue3/src/views/room/report/index.vue
@@ -48,9 +48,6 @@
              class="!w-210px"
            />
          </el-form-item>
-         <el-text size="large">
-          报警次数：{{ cabinetInfo.alarm }}
-         </el-text>
          
        </el-form>
       <el-collapse  v-model="activeNames">
@@ -58,27 +55,29 @@
           <el-row class="text-container"> 
             <el-col :span="8" >
               <div style="font-size: 30px;">
-                机柜名称 {{ cabinetInfo.name }}<br/>
-                类型 {{ cabinetInfo.type }}<br/>
-                容量 {{ cabinetInfo.pow_capacity }}kW<br/>
-                所属机房 {{ cabinetInfo.owner }} <br/>
-                平衡度 {{ cabinetInfo.balance }} <br/>
+                机房名称 {{ roomInfo.name }}<br/>
+                类型 {{ roomInfo.type }}<br/>
+                IT柜总数 {{ roomInfo.ITTotal }}<br/>
+                IT柜空闲数 {{ roomInfo.ITNotUse }}<br/>
+                IT柜报警数 {{ roomInfo.ITAlarm  }}<br/>
+                容量 {{ roomInfo.pow_capacity }}kW<br/>
               </div>
             </el-col>
             <el-col :span="6">
-              <div style="font-size: 28px;">
-                用电量 {{ cabinetInfo.eq }}kW<br/>
-                总视在功率(最大) {{ cabinetInfo.total_apparent_pow_max_value }}kWh  <br/>
-                最大温度 {{ cabinetInfo.tem_max_value }}°C<br/>
-                最大湿度 {{ cabinetInfo.hum_max_value }}%<br/>
-                负载百分比 {{ cabinetInfo.loadPer }}%<br/>
+              <div style="font-size: 29px;">
+                用电量 {{ roomInfo.eq }}kW<br/>
+                总视在功率(最大) {{ roomInfo.total_apparent_pow_max_value }}kWh  <br/>
+                最大温度 {{ roomInfo.tem_max_value }}°C<br/>
+                最大湿度 {{ roomInfo.hum_max_value }}%<br/>
+                平衡度 {{ roomInfo.balance }} <br/>
+                负载百分比 {{ roomInfo.loadPer }}%<br/>
               </div>
             </el-col> 
             <el-col :span="10">
               <el-row justify="center">
-                <el-col  :span="6">服务器IT总视在功率</el-col>
+                <el-col  :span="6">机房IT总视在功率</el-col>
                 <el-col  :span="6">&nbsp;</el-col>
-              </el-row>
+              </el-row>           
               <el-row>
                 <div ref="serChartContainer" id="serChartContainer" style="width: 23vw; height: 30vh"></div>  
               </el-row>
@@ -86,7 +85,7 @@
           </el-row>
          
         </el-collapse-item>
-        <el-collapse-item title="耗电排名" name="2">
+        <el-collapse-item title="机房耗电排名" name="2">
           <el-form-item label="颗粒度" prop="type">
            <el-select
              v-model="queryParams.eqGranularity"
@@ -98,11 +97,12 @@
              <el-option label="月" value="month" />
             </el-select>
           </el-form-item>
-
             <div ref="rankChartContainer" id="rankChartContainer" style="width: 75vw; height: 58vh;"></div>
-
         </el-collapse-item>
-        <el-collapse-item title="功率曲线" name="3">
+        <el-collapse-item title="机柜列耗电排名" name="3">
+            <div ref="cabinetRankChartContainer" id="cabinetRankChartContainer" style="width: 75vw; height: 58vh;"></div>
+        </el-collapse-item>
+        <el-collapse-item title="功率曲线" name="4">
           <el-form-item label="颗粒度" prop="type">
            <el-select
              v-model="queryParams.powGranularity"
@@ -118,7 +118,7 @@
             <div ref="powChartContainer" id="powChartContainer" style="width: 70vw; height: 58vh;"></div>
           </ContentWrap>
         </el-collapse-item>
-        <el-collapse-item title="温度曲线" name="4">
+        <el-collapse-item title="温度曲线" name="5">
           <el-form-item label="颗粒度" prop="type">
            <el-select
              v-model="queryParams.temGranularity"
@@ -142,15 +142,15 @@
 </template>
 
 <script setup lang="ts">
-import download from '@/utils/download'
-import { PDUDeviceApi, PDUDeviceVO } from '@/api/pdu/pdudevice'
+// import download from '@/utils/download'
+// import { PDUDeviceApi} from '@/api/pdu/pdudevice'
 import * as echarts from 'echarts';
 import { ElTree } from 'element-plus'
 
 /** PDU设备 列表 */
 defineOptions({ name: 'PDUDevice' })
 
-const activeNames = ref(["1","2","3","4"])
+const activeNames = ref(["1","2","3","4","5"])          
 
 const queryParams = reactive({
   pageNo: 1,
@@ -160,7 +160,7 @@ const queryParams = reactive({
   eqGranularity:"day",
   powGranularity : "hour",
   temGranularity : "hour",
-  ipAddr: undefined,
+  ipAddr: undefined, 
   createTime: undefined,
 })
 
@@ -168,72 +168,72 @@ const serverRoomArr =  [
   {
     value: '1',
     label: '机房1',
-    children: [
-      {
-        value: '1-1',
-        label: '母线1',
-        // children: [
-        // {
-        //   value: '1-1-1',
-        //   label: '机柜1',
-        // },
-        // {
-        //   value: '1-1-2',
-        //   label: '机柜2',
-        // },]
-      },
-    ],
+    // children: [ 
+    //   {
+    //     value: '1-1',
+    //     label: '母线1',
+    //     children: [
+    //     {
+    //       value: '1-1-1',
+    //       label: '机柜1',
+    //     },
+    //     {
+    //       value: '1-1-2',
+    //       label: '机柜2',
+    //     },]
+    //   },
+    // ],
   },
   {
     value: '2',
     label: '机房2',
-    children: [
-      {
-        value: '2-1',
-        label: '母线1',
-        // children: [
-        // {
-        //   value: '2-1-1',
-        //   label: '机柜1',
-        // },
-        // {
-        //   value: '2-1-2',
-        //   label: '机柜2',
-        // },]
-      },
-    ],
+    // children: [
+    //   {
+    //     value: '2-1',
+    //     label: '母线1',
+    //     children: [
+    //     {
+    //       value: '2-1-1',
+    //       label: '机柜1',
+    //     },
+    //     {
+    //       value: '2-1-2',
+    //       label: '机柜2',
+    //     },]
+    //   },
+    // ],
   },
   {
     value: '3',
     label: '机房3',
-    children: [
-      {
-        value: '3-1',
-        label: '母线1',
-        // children: [
-        // {
-        //   value: '3-1-1',
-        //   label: '机柜1',
-        // },
-        // {
-        //   value: '3-1-2',
-        //   label: '机柜2',
-        // },]
-      },
-      {
-        value: '3-2',
-        label: '母线2',
-        // children: [
-        // {
-        //   value: '3-1-1',
-        //   label: '机柜1',
-        // },
-        // {
-        //   value: '3-1-2',
-        //   label: '机柜2',
-        // },]
-      },
-    ],
+    // children: [
+    //   {
+    //     value: '3-1',
+    //     label: '母线1',
+    //     children: [
+    //     {
+    //       value: '3-1-1',
+    //       label: '机柜1',
+    //     },
+    //     {
+    //       value: '3-1-2',
+    //       label: '机柜2',
+    //     },]
+    //   },
+    //   {
+    //     value: '3-2',
+    //     label: '母线2',
+    //     children: [
+    //     {
+    //       value: '3-1-1',
+    //       label: '机柜1',
+    //     },
+    //     {
+    //       value: '3-1-2',
+    //       label: '机柜2',
+    //     },]
+    //   },
+    // ],
   },
 ]
 
@@ -242,12 +242,13 @@ let treeWidth = ref(3)
 let isCollapsed = ref(0);
 
 
-const cabinetInfo = ref({
-  alarm : 6,
-  name : "机柜1",
+const roomInfo = ref({
+  name : "机房1",
   type : "xx",
-  pow_capacity : "250",
-  owner :"机房1",
+  ITTotal : 200,
+  ITNotUse : 20,
+  ITAlarm : 10,
+  pow_capacity : "2500",
   eq : "200",
   total_apparent_pow_max_value : "200",
   tem_max_value : "25",
@@ -292,16 +293,25 @@ interface ServerData {
 }
 const serverData = ref<ServerData>({
   nameAndMax : [
-    { name: '服务器1', max: 250 },
-    { name: '服务器2', max: 250 },
-    { name: '服务器3', max: 250 },
-    { name: '服务器4', max: 250 },
-    { name: '服务器5', max: 250 },
-    { name: '服务器6', max: 250 },
-    { name: '服务器7', max: 250 },
-    { name: '服务器8', max: 250 }
+    { name: '机柜列1', max: 250 },
+    { name: '机柜列2', max: 250 },
+    { name: '机柜列3', max: 250 },
+    { name: '机柜列4', max: 250 },
+    { name: '机柜列5', max: 250 },
+    { name: '机柜列6', max: 250 },
+    { name: '机柜列7', max: 250 },
+    { name: '机柜列8', max: 250 }
   ],
   value: [200, 180, 170, 220, 167, 189,200,150]
+})
+
+interface CabinetEqData {
+  eq: number[];
+  name: string[];
+}
+const cabinetEqData = ref<CabinetEqData>({
+  eq : [],
+  name : []
 })
 
 
@@ -333,6 +343,8 @@ const getList = () => {
           location: "机房1-机柜1",
           eq: 501,
           eqCreateTime: "2024-03-13 08:00:00",
+          cabinetEq: 503,
+          cabinetName : "机柜列3",
           total_apparent_pow_avg_value : 200,
           total_active_pow_avg_value : 180,
           powCreateTime : "2024-03-13 08:00:00",
@@ -344,6 +356,8 @@ const getList = () => {
           location: "机房1-机柜1",
           eq: 435,
           eqCreateTime: "2024-03-05 08:00:00",
+          cabinetEq: 486,
+          cabinetName : "机柜列1",
           total_apparent_pow_avg_value : 190,
           total_active_pow_avg_value : 170,
           powCreateTime : "2024-03-13 09:00:00",
@@ -355,6 +369,8 @@ const getList = () => {
           location: "机房1-机柜1",
           eq: 360,
           eqCreateTime: "2024-02-15 08:00:00",
+          cabinetEq: 432,
+          cabinetName : "机柜列6",
           total_apparent_pow_avg_value : 220,
           total_active_pow_avg_value : 200,
           powCreateTime : "2024-03-13 10:00:00",
@@ -366,6 +382,8 @@ const getList = () => {
           location: "机房1-机柜1",
           eq: 352,
           eqCreateTime: "2024-03-20 08:00:00",
+          cabinetEq: 401,
+          cabinetName : "机柜列2",
           total_apparent_pow_avg_value : 170,
           total_active_pow_avg_value : 160,
           powCreateTime : "2024-03-13 11:00:00",
@@ -377,6 +395,8 @@ const getList = () => {
           location: "机房1-机柜1",
           eq: 290,
           eqCreateTime: "2024-02-05 08:00:00",
+          cabinetEq: 386,
+          cabinetName : "机柜列5",
           total_apparent_pow_avg_value : 200,
           total_active_pow_avg_value : 180,
           powCreateTime : "2024-03-13 12:00:00",
@@ -388,6 +408,8 @@ const getList = () => {
           location: "机房1-机柜1",
           eq: 275,
           eqCreateTime: "2024-02-25 08:00:00",
+          cabinetEq: 332,
+          cabinetName : "机柜列7",
           total_apparent_pow_avg_value : 200,
           total_active_pow_avg_value : 180,
           powCreateTime : "2024-03-13 13:00:00",
@@ -399,6 +421,8 @@ const getList = () => {
           location: "机房1-机柜1",
           eq: 260,
           eqCreateTime: "2024-03-25 08:00:00",
+          cabinetEq: 296,
+          cabinetName : "机柜列4",
           total_apparent_pow_avg_value : 200,
           total_active_pow_avg_value : 180,
           powCreateTime : "2024-03-13 14:00:00",
@@ -406,9 +430,9 @@ const getList = () => {
           temCrateTime : "2024-03-13 14:00:00",
       },
     ];
-
+    
     eqData.value.eq = fakeData.map((item) => item.eq);
-    eqData.value.time = fakeData.map((item) => item.eqCreateTime);
+    eqData.value.time = fakeData.map((item) => item.eqCreateTime); 
 
     powData.value.total_apparent_pow_avg_value = fakeData.map((item) => item.total_apparent_pow_avg_value);
     powData.value.total_active_pow_avg_value = fakeData.map((item) => item.total_active_pow_avg_value);
@@ -416,6 +440,9 @@ const getList = () => {
 
     temData.value.tem_avg_value = fakeData.map((item) => item.tem_avg_value);
     temData.value.time = fakeData.map((item) => item.temCrateTime);
+
+    cabinetEqData.value.eq = fakeData.map((item) => item.cabinetEq);
+    cabinetEqData.value.name = fakeData.map((item) => item.cabinetName);
   }finally{
     loading.value = false
   }
@@ -429,6 +456,8 @@ const temChartContainer = ref<HTMLElement | null>(null);
 let temChart = null as echarts.ECharts | null; // 显式声明 temChart 的类型
 const serChartContainer = ref<HTMLElement | null>(null);
 let serChart = null as echarts.ECharts | null; // 显式声明 serChart 的类型
+const cabinetRankChartContainer = ref<HTMLElement | null>(null);
+let cabinetChart = null as echarts.ECharts | null; // 显式声明 cabinetChart 的类型
 
 const initChart = () => {
   const instance = getCurrentInstance();
@@ -448,6 +477,23 @@ const initChart = () => {
     });
     // 将 rankChart 绑定到组件实例，以便在销毁组件时能够正确释放资源
     instance.appContext.config.globalProperties.rankChart = rankChart;
+  }
+  if (cabinetRankChartContainer.value && instance) {
+    cabinetChart = echarts.init(cabinetRankChartContainer.value);
+    cabinetChart.setOption({
+      // 这里设置 Echarts 的配置项和数据
+      title: { text: ''},
+      tooltip: { trigger: 'axis'},
+      legend: { data: ['耗电量']},
+      toolbox: {feature: {saveAsImage: {} }},
+      xAxis: {type: 'category' ,data:cabinetEqData.value.name},
+      yAxis: { type: 'value'},
+      series: [
+        {name:"耗电量",  type: 'bar', data: cabinetEqData.value.eq, label: { show: true, position: 'top' }},// 你可以根据需要选择标签的位置，比如 'top', 'insideTop', 'inside', 等等
+      ],
+    });
+    // 将 cabinetChart 绑定到组件实例，以便在销毁组件时能够正确释放资源
+    instance.appContext.config.globalProperties.cabinetChart = cabinetChart;
   }
   if (powChartContainer.value && instance) {
     powChart = echarts.init(powChartContainer.value);
@@ -505,22 +551,23 @@ const beforeRankUnmount = () => {
     rankChart?.dispose(); // 销毁图表实例
 };
 
-const beforePowUnmount = () => {
-    powChart?.dispose();  // 销毁图表实例
-};
+// const beforePowUnmount = () => {
+//     powChart?.dispose();  // 销毁图表实例
+// };
 
-const beforeTemUnmount = () => {
-    temChart?.dispose(); // 销毁图表实例
-};
+// const beforeTemUnmount = () => {
+//     temChart?.dispose(); // 销毁图表实例
+// };
 
-const beforeSerUnmount = () => {
-    serChart?.dispose(); // 销毁图表实例
-};
+// const beforeSerUnmount = () => {
+//     serChart?.dispose(); // 销毁图表实例
+// };
 
 window.addEventListener('resize', function() {
     rankChart?.resize(); 
     powChart?.resize(); 
     temChart?.resize(); 
+    cabinetChart?.resize(); 
 });
 
 watch(filterText, (val) => {
@@ -528,7 +575,8 @@ watch(filterText, (val) => {
 })
 
 // 监听类型颗粒度
-watch([ () => queryParams.eqGranularity, () => queryParams.powGranularity, () => queryParams.temGranularity], (eqNew,powNew,temNew) => {
+//eqNew,powNew,temNew
+watch([ () => queryParams.eqGranularity, () => queryParams.powGranularity, () => queryParams.temGranularity], (eqNew) => {
     const [ newEqGranularity] = eqNew;
     // const [ newPowGranularity] = powNew;
     // const [ newTemGranularity] = temNew;
@@ -601,51 +649,51 @@ watch([ () => queryParams.eqGranularity, () => queryParams.powGranularity, () =>
     
 });
 
-// 下拉框选项数组
-const deviceStatus = ref([])
+// // 下拉框选项数组
+// const deviceStatus = ref([])
 
-const message = useMessage() // 消息弹窗
-const { t } = useI18n() // 国际化
+// const message = useMessage() // 消息弹窗
+// const { t } = useI18n() // 国际化
 
 const toggleCollapse = () => {
   treeWidth.value = isCollapsed.value == 0 ? 3 : 0;
 };
 
 const loading = ref(false) // 列表的加载中
-const list = ref([
-  { 
-    id:"1",
-    status:"空闲设备",
-    totalApparentPower:"200kW",
-    totalActivePower:"210kVA",
-    totalElectricalEnergy:"10.112kWh",
-    ipAddr:"192.168.1.1-0",
-    location:"机房2-机柜1-A路",
-    updateTime:"15:25:00"
-  },
-  { 
-    id:"2",
-    status:"离线设备",
-    totalApparentPower:"200kW",
-    totalActivePower:"210kVA",
-    totalElectricalEnergy:"10.112kWh",
-    ipAddr:"192.168.1.2-1",
-    location:"机房2-机柜2-B路",
-    updateTime:"15:25:00"
-  },{ 
-    id:"3",
-    status:"未绑定设备",
-    totalApparentPower:"200kW",
-    totalActivePower:"210kVA",
-    totalElectricalEnergy:"10.112kWh",
-    ipAddr:"192.168.1.3-2",
-    location:"机房2-机柜3-C路",
-    updateTime:"15:25:00"
-  },
-]) // 列表的数据
-const total = ref(0) // 列表的总页数
+// const list = ref([
+//   { 
+//     id:"1",
+//     status:"空闲设备",
+//     totalApparentPower:"200kW",
+//     totalActivePower:"210kVA",
+//     totalElectricalEnergy:"10.112kWh",
+//     ipAddr:"192.168.1.1-0",
+//     location:"机房2-机柜1-A路",
+//     updateTime:"15:25:00"
+//   },
+//   { 
+//     id:"2",
+//     status:"离线设备",
+//     totalApparentPower:"200kW",
+//     totalActivePower:"210kVA",
+//     totalElectricalEnergy:"10.112kWh",
+//     ipAddr:"192.168.1.2-1",
+//     location:"机房2-机柜2-B路",
+//     updateTime:"15:25:00"
+//   },{ 
+//     id:"3",
+//     status:"未绑定设备",
+//     totalApparentPower:"200kW",
+//     totalActivePower:"210kVA",
+//     totalElectricalEnergy:"10.112kWh",
+//     ipAddr:"192.168.1.3-2",
+//     location:"机房2-机柜3-C路",
+//     updateTime:"15:25:00"
+//   },
+// ]) // 列表的数据
+// const total = ref(0) // 列表的总页数
 const queryFormRef = ref() // 搜索的表单
-const exportLoading = ref(false) // 导出的加载中
+// const exportLoading = ref(false) // 导出的加载中
 
 /** 查询列表 */
 // const getList = async () => {
@@ -660,50 +708,50 @@ const exportLoading = ref(false) // 导出的加载中
 // }
 
 /** 搜索按钮操作 */
-const handleQuery = () => {
-  queryParams.pageNo = 1
-  // getList()
-}
+// const handleQuery = () => {
+//   queryParams.pageNo = 1
+//   // getList()
+// }
 
 /** 重置按钮操作 */
-const resetQuery = () => {
-  queryFormRef.value.resetFields()
-  handleQuery()
-}
+// const resetQuery = () => {
+//   queryFormRef.value.resetFields()
+//   handleQuery()
+// }
 
 /** 添加/修改操作 */
-const formRef = ref()
-const openForm = (type: string, id?: number) => {
-  formRef.value.open(type, id)
-}
+// const formRef = ref()
+// const openForm = (type: string, id?: number) => {
+//   formRef.value.open(type, id)
+// }
 
-/** 删除按钮操作 */
-const handleDelete = async (id: number) => {
-  try {
-    // 删除的二次确认
-    await message.delConfirm()
-    // 发起删除
-    await PDUDeviceApi.deletePDUDevice(id)
-    message.success(t('common.delSuccess'))
-    // 刷新列表
-    await getList()
-  } catch {}
-}
+// /** 删除按钮操作 */
+// const handleDelete = async (id: number) => {
+//   try {
+//     // 删除的二次确认
+//     await message.delConfirm()
+//     // 发起删除
+//     await PDUDeviceApi.deletePDUDevice(id)
+//     message.success(t('common.delSuccess'))
+//     // 刷新列表
+//     await getList()
+//   } catch {}
+// }
 
-/** 导出按钮操作 */
-const handleExport = async () => {
-  try {
-    // 导出的二次确认
-    await message.exportConfirm()
-    // 发起导出
-    exportLoading.value = true
-    const data = await PDUDeviceApi.exportPDUDevice(queryParams)
-    download.excel(data, 'PDU设备.xls')
-  } catch {
-  } finally {
-    exportLoading.value = false
-  }
-}
+// /** 导出按钮操作 */
+// const handleExport = async () => {
+//   try {
+//     // 导出的二次确认
+//     await message.exportConfirm()
+//     // 发起导出
+//     exportLoading.value = true
+//     const data = await PDUDeviceApi.exportPDUDevice(queryParams)
+//     download.excel(data, 'PDU设备.xls')
+//   } catch {
+//   } finally {
+//     exportLoading.value = false
+//   }
+// }
 
 /** 初始化 **/
 onMounted(() => {
