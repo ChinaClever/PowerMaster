edf7660dd25a5cdbfdd97eb9e326a3aed9f0c074
LWOHY <38808824+LWOHY@users.noreply.github.com>
2024-04-29 14:47:27 +0800

    新增机柜详情接口，修改机柜页面接口
    新增机柜详情接口，修改机柜页面接口

--- yudao-module-cabinet/yudao-module-cabinet-biz/src/main/java/cn/iocoder/yudao/module/cabinet/service/impl/CabinetServiceImpl.java
+++ yudao-module-cabinet/yudao-module-cabinet-biz/src/main/java/cn/iocoder/yudao/module/cabinet/service/impl/CabinetServiceImpl.java
@@ -1,8 +1,10 @@
 package cn.iocoder.yudao.module.cabinet.service.impl;
 
+import cn.iocoder.yudao.framework.common.entity.mysql.cabinet.CabinetIndex;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.module.cabinet.dto.CabinetIndexDTO;
 import cn.iocoder.yudao.module.cabinet.mapper.CabinetCfgMapper;
+import cn.iocoder.yudao.module.cabinet.mapper.CabinetIndexMapper;
 import cn.iocoder.yudao.module.cabinet.service.CabinetService;
 import cn.iocoder.yudao.module.cabinet.vo.CabinetIndexVo;
 import com.alibaba.fastjson2.JSON;
@@ -33,56 +35,54 @@ public class CabinetServiceImpl implements CabinetService {
     @Autowired
     CabinetCfgMapper cabinetCfgMapper;
     @Autowired
+    CabinetIndexMapper cabinetIndexMapper;
+    @Autowired
     RedisTemplate redisTemplate;
 
 
     @Override
-    public PageResult<CabinetIndexDTO> getPageCabinet(CabinetIndexVo vo) {
+    public PageResult<JSONObject> getPageCabinet(CabinetIndexVo vo) {
         try {
             Page<CabinetIndexDTO>  page = new Page<>(vo.getPageNo(), vo.getPageSize());
             //获取机柜列表
             Page<CabinetIndexDTO> indexDTOPage = cabinetCfgMapper.selectList(page,vo);
-            List<CabinetIndexDTO> result = new ArrayList<>();
+            List<JSONObject> result = new ArrayList<>();
             //获取redis 实时数据
             if (Objects.nonNull(indexDTOPage) && !CollectionUtils.isEmpty(indexDTOPage.getRecords())){
                 indexDTOPage.getRecords().forEach(dto -> {
                     String key = REDIS_KEY_CABINET + dto.getRoomId()+SPLIT_KEY + dto.getId();
                     JSONObject jsonObject = JSON.parseObject(JSON.toJSONString(redisTemplate.opsForValue().get(key)));
-                    JSONObject cabinetPower = jsonObject.getJSONObject(CABINET_POWER);
-
-                    if (cabinetPower.containsKey(PATH_A)) {
-
-                        JSONObject aPath = cabinetPower.getJSONObject(PATH_A);
-
-                        dto.setApparentA(aPath.getFloat(POW_APPARENT));
-                        dto.setActiveA(aPath.getFloat(POW_ACTIVE));
-                        dto.setEleA(aPath.getDouble(ELE_ACTIVE));
-
-                    }
-                    if (cabinetPower.containsKey(PATH_B)) {
-                        JSONObject bPath = cabinetPower.getJSONObject(PATH_B);
-
-                        dto.setApparentB(bPath.getFloat(POW_APPARENT));
-                        dto.setActiveB(bPath.getFloat(POW_ACTIVE));
-                        dto.setEleA(bPath.getDouble(ELE_ACTIVE));
-                    }
-                    if (cabinetPower.containsKey(TOTAL_DATA)) {
-                        JSONObject total = cabinetPower.getJSONObject(TOTAL_DATA);
-
-                        dto.setApparentTotal(total.getFloatValue(POW_APPARENT));
-                        dto.setActiveTotal(total.getFloatValue(POW_ACTIVE));
-                        dto.setEleA(total.getDouble(ELE_ACTIVE));
-                    }
-                    result.add(dto);
+                    result.add(jsonObject);
                 });
 
             }
 
-            return new PageResult<CabinetIndexDTO>(result, indexDTOPage.getTotal());
+            return new PageResult<JSONObject>(result, indexDTOPage.getTotal());
         }catch (Exception e){
             log.error("获取数据失败：",e);
         }
-        return new PageResult<CabinetIndexDTO>(new ArrayList<>(), 0L);
+        return new PageResult<JSONObject>(new ArrayList<>(), 0L);
 
+    }
+
+    @Override
+    public JSONObject getCabinetDetail(int id) {
+
+        try{
+
+            CabinetIndex index = cabinetIndexMapper.selectById(id);
+
+            String key = REDIS_KEY_CABINET + index.getRoomId()+SPLIT_KEY + id;
+            JSONObject jsonObject = JSON.parseObject(JSON.toJSONString(redisTemplate.opsForValue().get(key)));
+            return jsonObject;
+        }catch (Exception e){
+            log.error("获取基础数据失败: ",e);
+        }
+        return new JSONObject();
+    }
+
+    @Override
+    public int insertCabinet(CabinetIndexVo vo) {
+        return 0;
     }
 }
