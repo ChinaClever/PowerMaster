1e9518490b73bea002afae7e24ab26d35a033e4e
LWOHY <38808824+LWOHY@users.noreply.github.com>
2024-04-29 12:57:03 +0800

    修改接口
    修改接口

--- yudao-module-cabinet/yudao-module-cabinet-biz/src/main/java/cn/iocoder/yudao/module/cabinet/mapper/CabinetCfgMapper.java
+++ yudao-module-cabinet/yudao-module-cabinet-biz/src/main/java/cn/iocoder/yudao/module/cabinet/mapper/CabinetCfgMapper.java
@@ -3,6 +3,7 @@ package cn.iocoder.yudao.module.cabinet.mapper;
 import cn.iocoder.yudao.framework.common.entity.mysql.cabinet.CabinetCfg;
 import cn.iocoder.yudao.module.cabinet.dto.CabinetIndexDTO;
 import cn.iocoder.yudao.module.cabinet.vo.CabinetIndexVo;
+import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,5 +18,6 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface CabinetCfgMapper extends BaseMapper<CabinetCfg> {
 
+    @InterceptorIgnore(tenantLine = "true")
     Page<CabinetIndexDTO> selectList(Page<CabinetIndexDTO> page, @Param("indexVo")CabinetIndexVo indexVo);
 }
