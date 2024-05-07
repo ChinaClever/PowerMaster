1b3715bed19a181ae09471a8b05a5931891d7074
quanzhiwei <1945525565@qq.com>
2024-05-07 13:30:38 +0800

    update: 机柜状态交互和机柜详情修改

--- ui/yudao-ui-admin-vue3/src/components/NavTree/NavTree.vue
+++ ui/yudao-ui-admin-vue3/src/components/NavTree/NavTree.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div v-if="showSearch" class="head-container">
-      <el-input v-model="deptName" class="mb-20px" clearable placeholder="请输入部门名称">
+      <el-input v-model="deptName" class="mb-20px" clearable placeholder="请输入">
         <template #prefix>
           <Icon icon="ep:search" />
         </template>
@@ -15,6 +15,8 @@
         :accordion="isAccordion"
         :filter-node-method="filterNode"
         :show-checkbox="showCheckbox"
+        node-key="id"
+        :default-checked-keys="checkKeys"
         @node-click="handleNodeClick"
         @check="handleCheckedNodes" />
     </div>
@@ -25,11 +27,13 @@
 import { ElTree } from 'element-plus'
 import * as DeptApi from '@/api/system/dept'
 import { defineProps } from 'vue'
+import type Node from 'element-plus/es/components/tree/src/model/node'
 
 const defaultProps = {
   children: 'children',
-  label: 'label'
+  label: 'name'
 }
+const checkKeys = ref<number[]>([])
 const deptName = ref('')
 const emits = defineEmits(['node-click', 'check'])
 const props = defineProps({
@@ -46,10 +50,16 @@ const props = defineProps({
 })
 const treeRef = ref<InstanceType<typeof ElTree>>()
 
+/** 复选初始化 */
+const initCheck = (keys) => {
+  treeRef.value!.setCheckedKeys(keys)
+  console.log('initCheck')
+}
+
 /** 基于名字过滤 */
 const filterNode = (value:string, data) => {
   if (!value) return true
-  return data.label.includes(value)
+  return data.name.includes(value)
 }
 
 /** 处理部门被点击 */
@@ -59,9 +69,9 @@ const handleNodeClick = async (row: { [key: string]: any }) => {
 }
 
 /** 处理多选被点击 */
-const handleCheckedNodes = async (checkedNodes, checkedKeys, halfCheckedNodes, halfCheckedKeys) => {
-  emits('check', checkedNodes)
-  console.log('处理多选被点击', checkedNodes, checkedKeys, halfCheckedNodes, halfCheckedKeys)
+const handleCheckedNodes = async (checkedNodes, checkedKeys) => {
+  emits('check', treeRef.value?.getCheckedNodes())
+  console.log('处理多选被点击', checkedNodes, checkedKeys, treeRef.value?.getCheckedNodes())
 }
 
 /** 监听deptName */
@@ -69,11 +79,14 @@ watch(deptName, (val) => {
   treeRef.value!.filter(val)
 })
 
+defineExpose({ initCheck })
+
 </script>
 
 <style scoped>
 /* 自定义按钮组件的样式 */
 .container {
+  width: 100%;
   padding: 20px;
   box-sizing: border-box;
 }
