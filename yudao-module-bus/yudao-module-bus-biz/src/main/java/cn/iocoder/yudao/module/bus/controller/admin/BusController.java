707e156378a714ccda2f4c2c028888f4e1673721
LWOHY <38808824+LWOHY@users.noreply.github.com>
2024-05-30 15:33:25 +0800

    修改

--- yudao-module-bus/yudao-module-bus-biz/src/main/java/cn/iocoder/yudao/module/bus/controller/admin/BusController.java
+++ yudao-module-bus/yudao-module-bus-biz/src/main/java/cn/iocoder/yudao/module/bus/controller/admin/BusController.java
@@ -7,6 +7,7 @@ import cn.iocoder.yudao.module.bus.service.BusService;
 import cn.iocoder.yudao.module.bus.vo.BusIndexVo;
 import com.alibaba.fastjson2.JSONObject;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
  * @description: 母线业务
  * @date 2024/5/30 14:34
  */
+@Tag(name = "管理后台 - 母线拓扑")
 @RestController
 public class BusController {
 
