34b0d95aa0e9663d7d1e36eca0d081f8946efa06
LW <38808824+LW@users.noreply.github.com>
2024-08-20 11:02:54 +0800

    更改类型

--- yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/entity/mysql/cabinet/CabinetBus.java
+++ yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/entity/mysql/cabinet/CabinetBus.java
@@ -47,12 +47,12 @@ public class CabinetBus implements Serializable {
     /**
      * 标记位-名字  前端用
      */
-    private Integer boxIndexA;
+    private String boxIndexA;
 
     /**
      * 标记位-名字  前端用
      */
-    private Integer boxIndexB;
+    private String boxIndexB;
 
     /**
      * 更新时间
