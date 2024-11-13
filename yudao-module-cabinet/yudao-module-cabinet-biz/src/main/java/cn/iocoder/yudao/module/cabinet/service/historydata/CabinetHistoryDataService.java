b01d133442d86b5b91e604f6a04046b9661b0c20
jiangjinchi <15577363341@163.com>
2024-11-13 11:02:59 +0800

    机柜实时能耗接口

--- yudao-module-cabinet/yudao-module-cabinet-biz/src/main/java/cn/iocoder/yudao/module/cabinet/service/historydata/CabinetHistoryDataService.java
+++ yudao-module-cabinet/yudao-module-cabinet-biz/src/main/java/cn/iocoder/yudao/module/cabinet/service/historydata/CabinetHistoryDataService.java
@@ -1,5 +1,6 @@
 package cn.iocoder.yudao.module.cabinet.service.historydata;
 
+import cn.iocoder.yudao.framework.common.entity.mysql.room.RoomIndex;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.module.cabinet.controller.admin.historydata.vo.CabinetHistoryDataDetailsReqVO;
 import cn.iocoder.yudao.module.cabinet.controller.admin.historydata.vo.CabinetHistoryDataPageReqVO;
@@ -38,4 +39,6 @@ public interface CabinetHistoryDataService {
     List<Object> getNewHistoryList(List<Object> list);
 
     List<Object>  getNewDetailHistoryList(List<Object> list);
+
+    Map<Integer, RoomIndex> getRoomById(List<Integer> roomIds);
 }
