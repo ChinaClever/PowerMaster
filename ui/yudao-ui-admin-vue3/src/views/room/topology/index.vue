8702dfe858118a394bbecbebb56ad5e7a3d04743
hanhanxiaochen <839012837@qq.com>
2024-11-27 16:31:55 +0800

    机房选择， 编辑 对应机房
    机房选择， 编辑 对应机房

--- ui/yudao-ui-admin-vue3/src/views/room/topology/index.vue
+++ ui/yudao-ui-admin-vue3/src/views/room/topology/index.vue
@@ -185,6 +185,14 @@ const chosenBtn = ref(0)
 const ContainerHeight = ref(100)
 const loading = ref(false)
 const movingInfo = ref<any>({})
+
+const roomDownValId = ref();
+
+const roomsId = reactive({
+  roomDownValIds: history?.state?.id,
+})
+
+
 const rowColInfo = reactive({
   roomName: '', // 机房名
   row: 14, // 行
@@ -581,7 +589,11 @@ const getRoomList = async() => {
     roomList.value = res
     const find = res.find(item => item.id == roomId.value)
     if (!find) {
-      roomId.value = res[0].id
+      if(roomsId.roomDownValIds == null){
+          roomId.value = res[0].id
+      }else{
+          roomId.value = roomsId.roomDownValIds;
+      }
     }
     emit('getroomid', roomId.value)
     getRoomInfo()
@@ -597,6 +609,7 @@ const getRoomInfo = async() => {
     const result2 = MachineRoomApi.getRoomDataDetail({id: roomId.value})
     const results = await Promise.all([result1, result2])
     const res = results[0]
+    roomDownValId.value = res.id;
     console.log('res', res)
     const data = [] as any
     const Obj = {}
@@ -756,8 +769,9 @@ const handleDelete = () => {
 }
 // 处理点击编辑事件
 const handleEdit = () => {
+  alert(roomDownValId.value)
   if (isFromHome) {
-    push('/room/topology')
+    push({path: '/room/topology', state: { id:roomDownValId.value}})
     return
   }
   editEnable.value = true
