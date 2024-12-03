fd6f213c579a18bbbff8bb7eeb9a03adaeb1c831
quanzhiwei <1945525565@qq.com>
2024-12-03 15:21:02 +0800

    母线能耗-实时能耗（插接箱）

--- ui/yudao-ui-admin-vue3/src/views/bus/eleTotalRealtime/bus.vue
+++ ui/yudao-ui-admin-vue3/src/views/bus/eleTotalRealtime/bus.vue
@@ -281,9 +281,9 @@ function customTooltipFormatter(params: any[]) {
   tooltipContent += '位置：'+list.value[item.dataIndex].location + '<br/>'+
                     item.marker +'设备地址：'+list.value[item.dataIndex].devKey+'<br/>'+
                     item.marker +'设备名称：'+list.value[item.dataIndex].busName+'<br/>'
-                    +item.marker + '开始电能：'+formatEle(null, null, list.value[item.dataIndex].eleActiveStart)  + 'kWh 开始日期：'+formatTime(null, null, list.value[item.dataIndex].createTimeMin) + '<br/>' 
-                    +item.marker +'结束电能：'+formatEle(null, null, list.value[item.dataIndex].eleActiveEnd) + 'kWh 结束日期：'+formatTime(null, null, list.value[item.dataIndex].createTimeMax) + '<br/>'
-                    +item.marker +'耗电量：'+formatEle(null, null, list.value[item.dataIndex].eleActive) + 'kWh';
+                    +item.marker + '开始日期：' + formatTime(null, null, list.value[item.dataIndex].createTimeMin) + '开始电能：' + formatEle(null, null, list.value[item.dataIndex].eleActiveStart)  + 'kWh ' + '<br/>' 
+                    +item.marker + '结束日期：' + formatTime(null, null, list.value[item.dataIndex].createTimeMax) + '结束电能：' + formatEle(null, null, list.value[item.dataIndex].eleActiveEnd) + 'kWh '+ '<br/>'
+                    +item.marker +'耗电量：'+ formatEle(null, null, list.value[item.dataIndex].eleActive) + 'kWh';
   return tooltipContent;
 }
 
