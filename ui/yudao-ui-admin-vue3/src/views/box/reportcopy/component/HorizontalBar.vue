b5bd0408edfce8d42d5b89f6e5fd9ecb21a16b7b
jiangjinchi <15577363341@163.com>
2025-02-12 09:27:24 +0800

    插接箱报表输出位问题

--- ui/yudao-ui-admin-vue3/src/views/box/reportcopy/component/HorizontalBar.vue
+++ ui/yudao-ui-admin-vue3/src/views/box/reportcopy/component/HorizontalBar.vue
@@ -10,61 +10,64 @@ const prop = defineProps({
     required: true
   },
   height: {
-    type: [Number,String],
+    type: [Number, String],
     default: 60
   },
   width: {
-    type: [Number,String],
+    type: [Number, String],
     default: 60
   }
 })
 
-const series = ref({});
-
+const series = ref([]);
 const time = ref([]);
 const legendList = ref([]);
+const count = ref([]);
 
-const count = ref({});
-const length = ref();
+// 检查 prop.list 是否为空
+if (Object.keys(prop.list).length === 0) {
+  console.warn('prop.list is empty');
+} else {
+  const keys = Object.keys(prop.list);
+  const length = keys.length;
 
-// 初始化 count 的每个属性为数组
-for (let i = 1; i < Object.keys(prop.list).length; i++) {
-  count.value[i] = ref([]); // 或者直接使用 []，如果不需要深度响应式
-}
-for (let i = 1; i < Object.keys(prop.list).length; i++) {
-  count.value[i].push(...prop.list[i].map(item => item.pow_active_avg_value));
-}
+  // 初始化 count 数组
+  for (let i = 0; i < length; i++) {
+    count.value.push([]);
+    if (Array.isArray(prop.list[keys[i]])) {
+      count.value[i].push(...prop.list[keys[i]].map(item => item.pow_active_avg_value));
+    }
+  }
 
-for (let i = 1; i < (Object.keys(prop.list).length); i++) {
-  series.value[i - 1] = {
-    name: `输出位${i}`,
-    data: count.value[i],
-    type: 'line',
+  // 构建 series 和 legendList
+  for (let i = 0; i < length-1; i++) {
+    series.value.push({
+      name: `输出位${i + 1}`,
+      data: count.value[i],
+      type: 'line',
+      symbol: 'circle',
+      symbolSize: 4
+    });
+    legendList.value.push(`输出位${i + 1}`);
+  }
+
+  // 初始化时间轴数据
+  if (prop.list.time) {
+    time.value = prop.list.time;
   }
 }
 
-for (let i = 1; i < Object.keys(prop.list).length; i++) {
-  legendList.value.push(series.value[i - 1].name);
-}
-
-console.log('prop.list.length',Object.keys(prop.list).length);
-console.log('prop.list',prop.list);
-console.log('countcountvcountc',count.value[2]);
-console.log('series.value',series.value);
-console.log('legendList.value',legendList.value);
-console.log('time.value',time.value);
-
-const echartsOption = ref({
-  legend: { data:legendList.value },
+const echartsOption = computed(() => ({
+  legend: { data: legendList.value },
   xAxis: {
     type: 'category',
-    data: prop.list.time
+    data: time.value
   },
   yAxis: {
     type: 'value'
   },
   series: series.value
-})
+}));
 
 onUnmounted(() => {
   console.log('onUnmounted******')
