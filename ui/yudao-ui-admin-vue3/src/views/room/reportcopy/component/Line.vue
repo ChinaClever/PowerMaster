02abe49c244c6f00b756fbfabd2fda125389f879
chenwanyue <luozhiyong131@qq.com>
2025-04-27 15:29:12 +0800

    机房/机房报表-界面优化
    机房/机房报表-界面优化

--- ui/yudao-ui-admin-vue3/src/views/room/reportcopy/component/Line.vue
+++ ui/yudao-ui-admin-vue3/src/views/room/reportcopy/component/Line.vue
@@ -23,11 +23,6 @@ const series = ref()
 const time = ref()
 const legendList = ref()
 
-// 获取当前日期的年月日
-// const currentDate = ref('')
-// const today = new Date()
-// currentDate.value = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
-
 
 // 设置饼图的选项
 const echartsOption = ref({
@@ -57,7 +52,77 @@ const echartsOption = ref({
   },
   xAxis: {type: 'category', boundaryGap: false, data : time},
   yAxis: { type: 'value'},
-  toolbox: {feature: {saveAsImage: {},dataView:{},dataZoom :{},restore :{}, }},
+  toolbox: {feature: {saveAsImage: {},
+  // dataView:{}
+    // dataView: {
+  //     optionToContent: function(opt) {
+  //       // opt 是整个 echarts 的 option 对象
+  //       const axisData = opt.xAxis[0].data; // 获取x轴数据
+  //       const series = opt.series; // 获取所有系列数据
+        
+  //       // 构建HTML表格
+  //       let table = '<table style="width:100%;text-align:center"><tbody><tr><th>时间</th>';
+        
+  //       // 添加表头
+  //       series.forEach(item => {
+  //         table += `<th>${item.name}</th>`;
+  //       });
+  //       table += '</tr>';
+        
+  //       // 添加数据行
+  //       for (let i = 0; i < axisData.length; i++) {
+  //         table += `<tr><td>${axisData[i]}</td>`;
+  //         series.forEach(item => {
+  //           const value = item.data[i] !== undefined ? item.data[i].toFixed(3) : '-';
+  //           table += `<td>${value}</td>`;
+  //         });
+  //         table += '</tr>';
+  //       }
+        
+  //       table += '</tbody></table>';
+  //       return table;
+  //     }
+  //   },
+  dataView: {
+      optionToContent: function(opt) {
+        const axisData = opt.xAxis[0].data;
+        const series = opt.series;
+        
+        let table = `
+          <style>
+            .data-view-table {
+              width: 100%;
+              font-size: 12px;
+              border-collapse: collapse;
+            }
+            .data-view-table th, .data-view-table td {
+              padding: 4px 8px;
+              border: 1px solid #eee;
+              text-align: center;
+            }
+            .data-view-table th {
+              background: #fafafa;
+            }
+          </style>
+          <table class="data-view-table">
+            <tr><th>时间</th>
+        `;
+
+        series.forEach(item => table += `<th>${item.name}</th>`);
+        table += '</tr>';
+
+        for (let i = 0; i < axisData.length; i++) {
+          table += `<tr><td>${axisData[i]}</td>`;
+          series.forEach(item => {
+            table += `<td>${item.data[i]?.toFixed(3) || '-'}</td>`;
+          });
+          table += '</tr>';
+        }
+
+        return table + '</table>';
+      }
+    }
+  ,dataZoom :{},restore :{}, }},
   series: series,
 })
 
