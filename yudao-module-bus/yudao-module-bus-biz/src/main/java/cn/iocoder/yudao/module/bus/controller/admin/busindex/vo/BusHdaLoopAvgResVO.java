7916e8e7f6901df9d33f502f7d9d645d31d3e0f7
chenwanyue <luozhiyong131@qq.com>
2025-05-16 17:02:19 +0800

    母线报表/始端箱报表类修改
    母线报表/始端箱报表类修改

--- yudao-module-bus/yudao-module-bus-biz/src/main/java/cn/iocoder/yudao/module/bus/controller/admin/busindex/vo/BusHdaLoopAvgResVO.java
+++ yudao-module-bus/yudao-module-bus-biz/src/main/java/cn/iocoder/yudao/module/bus/controller/admin/busindex/vo/BusHdaLoopAvgResVO.java
@@ -14,19 +14,71 @@ public class BusHdaLoopAvgResVO {
 
     @Schema(description = "相")
     @JsonProperty("loop_id")
-    private Integer lineId;
+    private Integer loopId;
     /**
      *平均电压
      */
     @Schema(description = "平均电压")
     @JsonProperty("vol_avg_value")
-    private BigDecimal volAvgValue;
+    private Float volAvgValue;
+
+    /**
+     * 最大电压时间
+     */
+    @JsonProperty("vol_max_time")
+    private DateTime volMaxTime;
+
+
+    /**
+     * 最大电压
+     */
+    @JsonProperty("vol_max_value")
+    private Float volMaxValue;
+
+    /**
+     * 最小电压时间
+     */
+    @JsonProperty("vol_min_time")
+    private DateTime volMinTime;
+
+
+    /**
+     * 最小电压
+     */
+    @JsonProperty("vol_min_value")
+    private Float volMinValue;
+
     /**
      * 平均电流
      */
     @Schema(description = "平均电流")
     @JsonProperty("cur_avg_value")
-    private BigDecimal curAvgValue;
+    private Float curAvgValue;
+
+    /**
+     * 最大电流时间
+     */
+    @JsonProperty("cur_max_time")
+    private DateTime curMaxTime;
+
+    /**
+     * 最大电流
+     */
+    @JsonProperty("cur_max_value")
+    private Float curMaxValue;
+
+    /**
+     * 最小电流时间
+     */
+    @JsonProperty("cur_min_time")
+    private DateTime curMinTime;
+
+
+    /**
+     * 最小电流
+     */
+    @JsonProperty("cur_min_value")
+    private Float curMinValue;
 
 
     @Schema(description = "busId")
@@ -40,4 +92,7 @@ public class BusHdaLoopAvgResVO {
     @JsonProperty("create_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private DateTime createTime;
+
+
+
 }
