093add1e7ae8e2969910aa798ea3d4d747a2d2dc
chenwanyue <luozhiyong131@qq.com>
2025-05-26 17:20:02 +0800

    机房报表-功能完善
    机房报表-功能完善

--- yudao-module-room/yudao-module-room-biz/src/main/java/cn/iocoder/yudao/module/room/controller/admin/roomindex/vo/LineSeries.java
+++ yudao-module-room/yudao-module-room-biz/src/main/java/cn/iocoder/yudao/module/room/controller/admin/roomindex/vo/LineSeries.java
@@ -2,11 +2,15 @@ package cn.iocoder.yudao.module.room.controller.admin.roomindex.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class LineSeries extends  SeriesBase{
 
     private String type = "line";
 
     private String symbol = "none";
+
+    private List<String> happenTime;
 
 }
